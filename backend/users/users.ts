8e5bb32ffd41d0a6bd7fc912e39b8338f8d96368
seyicoded <opadonuseyi01@gmail.com>
2024-08-21 10:18:24 +0100

    schema declaration for convex

--- backend/users/users.ts
+++ backend/users/users.ts
@@ -1,5 +1,7 @@
-import { queryGeneric, mutationGeneric } from "convex/server";
+// @ts-nocheck
+import { queryGeneric, mutationGeneric, DefaultFunctionArgs } from "convex/server";
 import { v } from "convex/values";
+import { NonDocumentUser } from "src/types/user.types";
 
 export const getUsers = queryGeneric({
   args: {},
@@ -15,8 +17,57 @@ export const getUser = queryGeneric({
   },
 });
 
-export const createUser = mutationGeneric({
+export const updateOrCreateUser = mutationGeneric({
   handler: async (ctx, args) => {
-    return await ctx.db.insert("users", args);
+    const user = await ctx.db.query("users").filter(e => e.eq(e.field("primaryEmail"), args?.primaryEmail)).first();
+    if(user){
+      delete args?.primaryEmail;
+      await ctx.db.patch(user?._id, args);
+      return await ctx.db.get(user?._id);
+    }
+    return await ctx.db.insert("users", {
+      storageUsed: 0,
+      callMinuteUsed: 0,
+      callMinuteOverAll: 0,
+      lastCallTimeRefresh: (new Date()).toISOString(),
+      lastBoardId: 'unsorted',
+      accountType: 'Individual',
+      accountCategory: [],
+      accountDailyRoutine: [],
+      connectedEmails: [],
+      isAccountActive: true,
+      subscription: {
+        currentSubscription: 'free',
+        nextSubscription: 'free',
+        isCancelledRequest: false,
+        billingCircle: 30
+      },
+      stripe: {
+        hasAccount: false,
+        customer: {}
+      },
+      notification: {
+        device: false,
+        deviceToken: "",
+        email: true,
+        updateFrequency: "instantly",
+      },
+      hasUserSeenUpcomingTasks: false,
+      weekStartDay: "Sun",
+      timeFormat: "12",
+      lastTrayBoardDetails: {
+        note: {
+          title: "Unsorted",
+          id: "unsorted",
+        },
+        task: {
+          title: "Unsorted",
+          id: "unsorted",
+        },
+      },
+      hasSignedGoogle: false,
+      hasSignedMicrosoft: false,
+      ...args,
+    });
   },
 });
